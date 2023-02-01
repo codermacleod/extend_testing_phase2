6d6e7cc2752d32c7248e3f0408192549c909f176
Daniel Macleod <codermacleod@gmail.com>
2023-02-01 14:51:20 +0000

    Final commit for coaches

--- tests/document_test.py
+++ tests/document_test.py
@@ -1,13 +1,22 @@
 import os.path
 import random
+import shutil
+
 from faker import Faker
 
 fake = Faker('en-UK')
 
 # Paths
-finals = '../target_directory/finals'
-originals = '../target_directory/originals'
-updates = '../target_directory/updates'
+finals_path = "../target_directory/finals"
+originals_path = "../target_directory/originals"
+updates_path = "../target_directory/updates"
+drop_path = "../target_directory/droplist"
+allow_path = "../target_directory/allowlist"
+
+
+def create_new_directory(directory):
+    path = os.path.join("../target_directory", directory)
+    os.mkdir(path)
 
 
 def file_inserter(directory, number_of_files, seed):
@@ -15,11 +24,12 @@ def file_inserter(directory, number_of_files, seed):
         Faker.seed(seed)
         name = fake.name()
         surname = name.split()[1]
+        if directory == "updates":
+            Faker.seed(seed + 1)
         address = fake.address()
         file_object = open(f"../target_directory/{directory}/{surname}", 'a')
         file_object.write(name + '\n' + address)
         file_object.close()
-        # os.path.join(originals, f"{name}")
         print(name + '\n' + address)
         seed = seed + 1
 
@@ -36,7 +46,7 @@ def drop_or_allow_list_creator(drop_or_allow, number_of_people, seed_1):
             # read all content from a file using read()
             content = file.read()
             # check if string present or not
-            if surname in content:
+            if random.choice(name_list) in content:
                 print('string exist')
             else:
                 file_object.write(surname + '\n')
@@ -44,22 +54,26 @@ def drop_or_allow_list_creator(drop_or_allow, number_of_people, seed_1):
         seed_1 = seed_1 + 1
 
 
-file_inserter('originals', 5, 1)
-file_inserter('updates', 5, 6)
+def clear_all_directories_and_files():
+    directory_list = [originals_path, updates_path, finals_path]
+    drop_or_allow_list = [drop_path, allow_path]
 
-drop_or_allow_list_creator('drop', 5, 1)
-# 1. Create a directory for originals and add several documents to it.
-# 2. Create a droplist or allowlist file and add entries to it as per
-# the rules above.
-# 3. Create a directory for updates and add several documents to it.
-# 4. Run the program and check that the expected behavior is achieved.
-# 5. Check the new directory (finals) to ensure that the documents
-# present are the ones expected, according to the droplist/allowlist
-# file and the documents present in originals and updates.
-# 6. Check the documents in finals to ensure that the correct
-# version has been included (e.g. the document from updates
-# if a document exists in both originals and updates).
-# 7. Repeat the above steps with different documents,
-# droplist/allowlist files and different combinations
-# of documents in originals and updates to ensure that the
-# program behaves as expected in all scenarios.
+    for i in directory_list:
+        if os.path.exists(i):
+            shutil.rmtree(i)
+    for i in drop_or_allow_list:
+        if os.path.exists(i):
+            os.remove(i)
+
+
+clear_all_directories_and_files()
+
+create_new_directory("originals")
+create_new_directory("updates")
+create_new_directory("finals")
+
+file_inserter('originals', 5, 1)
+file_inserter('updates', 5, 1)
+
+drop_or_allow_list_creator('allow', 5, 1)
+# drop_or_allow_list_creator('drop', 5, 1)
